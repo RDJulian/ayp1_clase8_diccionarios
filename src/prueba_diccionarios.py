4e35ac133521172817095d411edcc9516eac4e67
RDJulian <jruizd@fi.uba.ar>
2024-01-24 13:09:00 -0300

    ref: Cambio en los ejercicios para el Curso 2024.

--- src/prueba_diccionarios.py
+++ src/prueba_diccionarios.py
@@ -1,35 +1,54 @@
 def main():
-    # Declarando el diccionario
+    # Instanciando el diccionario de forma explicita.
     alumno = {"Nombre": "Julian", "Apellido": "Ruiz Diaz"}
 
     # Imprimiendo el diccionario
-    print(alumno)
+    print(alumno, end="\n\n")
 
     # Alta
     alumno["Padron"] = 108411
-    print(alumno)
+    print(alumno, end="\n\n")
 
-    # Modificacion
+    # Consulta / Acceso
+    print(alumno["Padron"], end="\n\n")
+
+    # Modificación
     alumno["Padron"] = 108410
-    print(alumno)
+    # La clave "Padron" ya existía.
+    print(alumno, end="\n\n")
 
     # Baja
     del alumno["Padron"]
-    print(alumno)
+    print(alumno, end="\n\n")
 
-    # Iterando
-    for llave, valor in alumno.items():
-        print(f"{llave}: {valor}")
-
-    # Comparando
+    # Comparación
     alumno2 = {"Apellido": "Ruiz Diaz", "Nombre": "Julian"}
-    print(alumno == alumno2)
+    print(alumno == alumno2, end="\n\n")
 
-    # Copia (TENER MUCHO CUIDADO)
+    # ¿Copia? (TENER MUCHO CUIDADO)
     alumno2 = alumno
     alumno["Padron"] = 108415
     print(alumno)
-    print(alumno2)
+    print(alumno2, end="\n\n")
+
+    # Copia
+    alumno2 = alumno.copy()
+    alumno["Padron"] = 109000
+    print(alumno)
+    print(alumno2, end="\n\n")
+
+    # Demostración de las distintas iteraciones posibles:
+    for clave in alumno.keys():
+        print(clave)
+        print(alumno[clave])
+    print()
+
+    for valor in alumno.values():
+        print(valor)
+    print()
+
+    for clave, valor in alumno.items():
+        print(f"{clave} : {valor}")
 
 
 if __name__ == "__main__":
